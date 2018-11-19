93a2f05155314f6682173884131c3dc29b2573be
Travis Kohlbeck <me+git@travisk.info>
2018-11-18 21:42:27 -0500

    working list and text field

--- src/App.js
+++ src/App.js
@@ -1,31 +1,91 @@
-import React, { Component } from 'react'
-import Button from '@material-ui/core/Button'
-import logo from './logo.svg'
-import './App.css'
+import React, { Component, Fragment } from 'react'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import {
+  AppBar,
+  Button,
+
+  List,
+  ListItem,
+  ListItemSecondaryAction,
+  ListItemText,
+
+  TextField,
+  Toolbar,
+  Typography,
+} from '@material-ui/core'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: [],
+      text: '',
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(e) {
+    this.setState({ text: e.target.value })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+    if (!this.state.text.length) { return }
+
+    const newItem = {
+      text: this.state.text,
+      id: Date.now(),
+    }
+
+    this.setState(state => (
+      {
+        items: state.items.concat(newItem),
+        text: '',
+      }
+    ))
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <br />
-          <Button variant='outlined' color='primary'>
-            Add Datum
-          </Button>
-        </header>
-      </div>
+      <Fragment>
+        <CssBaseline />
+
+        <AppBar position='static'>
+          <Toolbar>
+            <Typography variant='title' color='inherit'>
+              Datum
+            </Typography>
+          </Toolbar>
+        </AppBar>
+
+        <List>
+          {this.state.items.map(item => (
+            <ListItem divider key={item.id}>
+              <ListItemText>
+                {item.text}
+              </ListItemText>
+            </ListItem>
+          ))}
+        </List>
+
+        <form onSubmit={this.handleSubmit} autoComplete='off'>
+          <TextField
+            id='new-todo'
+            label='New Datum'
+            value={this.state.text}
+            onChange={this.handleChange}
+            margin='dense'
+            variant='outlined'
+            style={{ margin: 8 }}
+          />
+        </form>
+
+        <Button style={{margin: 8}} variant='contained' color='primary'>
+          Add Datum
+        </Button>
+
+      </Fragment>
     );
   }
 }
