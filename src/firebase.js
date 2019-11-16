e097599b33950068f8d88f590efb2c861aa5047b
Travis Kohlbeck <me+git@travisk.info>
2019-11-16 15:01:50 -0500

    pulls out firebase api keys to secret file and env vars

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import * as firebase from 'firebase'
+import secret from './secrets'
+import 'firebase/firestore'
+		
+const config = secret || {
+  apiKey: process.env.FB_API_KEY,
+  authDomain: process.env.FB_AUTH_DOMAIN,
+  databaseURL: process.env.FB_DATABASE_URL,
+  projectId: process.env.FB_PROJECT_ID,
+  storageBucket: process.env.FB_STORAGE_BUCKET,
+  messagingSenderId: process.env.FB_MESSAGE_SENDER_ID,
+  appId: process.env.FB_APP_ID,
+}
+
+firebase.initializeApp(config)
+export default firebase
