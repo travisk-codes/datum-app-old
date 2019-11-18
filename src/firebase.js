c8c000afaea442d6c4130e9dbac4c04e91186200
Travis Kohlbeck <me+git@travisk.info>
2019-11-17 19:48:01 -0500

    gets db working possibly

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,17 @@
-import * as firebase from 'firebase'
-//import secret from './secrets'
-import 'firebase/firestore'
-		
-const config = {
-  apiKey: process.env.FB_API_KEY,
-  authDomain: process.env.FB_AUTH_DOMAIN,
-  databaseURL: process.env.FB_DATABASE_URL,
-  projectId: process.env.FB_PROJECT_ID,
-  storageBucket: process.env.FB_STORAGE_BUCKET,
-  messagingSenderId: process.env.FB_MESSAGE_SENDER_ID,
-  appId: process.env.FB_APP_ID,
+import * as firebase from 'firebase/app'
+import 'firebase/database'
+//import 'firebase/firestore'
+import 'firebase/auth'
+import secret from './secrets'
+
+const config = secret || {
+	apiKey: process.env.FB_API_KEY,
+	authDomain: process.env.FB_AUTH_DOMAIN,
+	databaseURL: process.env.FB_DATABASE_URL,
+	projectId: process.env.FB_PROJECT_ID,
+	storageBucket: process.env.FB_STORAGE_BUCKET,
+	messagingSenderId: process.env.FB_MESSAGE_SENDER_ID,
+	appId: process.env.FB_APP_ID,
 }
 
 firebase.initializeApp(config)
